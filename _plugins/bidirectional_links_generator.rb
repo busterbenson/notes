eb53d4f7efe4de5581e801a036b1c81c47c51ddd
Buster Benson <busterbenson@users.noreply.github.com>
2020-09-09 14:01:52 -0700

    adding a new post about 750 words v2

--- _plugins/bidirectional_links_generator.rb
+++ _plugins/bidirectional_links_generator.rb
@@ -30,29 +30,29 @@ class BidirectionalLinksGenerator < Jekyll::Generator
         e.content.include?(current_note.url)
       end
 
-      # Nodes: Graph
-      graph_nodes << {
-        id: note_id_from_note(current_note),
-        path: current_note.url,
-        label: current_note.data['title'],
-      } unless current_note.path.include?('_notes/index.html')
+      # # Nodes: Graph
+      # graph_nodes << {
+      #   id: note_id_from_note(current_note),
+      #   path: current_note.url,
+      #   label: current_note.data['title'],
+      # } unless current_note.path.include?('_notes/index.html')
 
       # Edges: Jekyll
       current_note.data['backlinks'] = notes_linking_to_current_note
 
-      # Edges: Graph
-      notes_linking_to_current_note.each do |n|
-        graph_edges << {
-          source: note_id_from_note(n),
-          target: note_id_from_note(current_note),
-        }
-      end
+      # # Edges: Graph
+      # notes_linking_to_current_note.each do |n|
+      #   graph_edges << {
+      #     source: note_id_from_note(n),
+      #     target: note_id_from_note(current_note),
+      #   }
+      # end
     end
 
-    File.write('_includes/notes_graph.json', JSON.dump({
-      edges: graph_edges,
-      nodes: graph_nodes,
-    }))
+    # File.write('_includes/notes_graph.json', JSON.dump({
+    #   edges: graph_edges,
+    #   nodes: graph_nodes,
+    # }))
   end
 
   def note_id_from_note(note)
